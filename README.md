6efa3293518f7a491f0c5fe7a9d6ee44d65dd1f5
NMods <lc3@null.net>
2020-01-16 22:10:15 +0200

    no more censor!

---
+++ README.md
@@ -0,0 +1,13 @@
+# No More Censor
+
+Tera Toolbox module
+
+Removes censoring from every chat message you see and every chat message you **send**.<br>
+In other words, you can swear all you want and others will see it.<br>
+You can even say things like `50 yard cunt punt`, `blue waffle` and `carpet muncher` :open_mouth:<br>
+Or use any of the 5000+ censored german words<br>
+<sub><sup>Or *erp to your heart's desire..*</sub></sup>
+
+**TESTED ON EU ONLY**
+
+Let me know if you encounter any problems
---
+++ index.js
@@ -0,0 +1,53 @@
+const DEFAULT_HOOK_SETTINGS = { order: -1002, filter: { fake: false } },
+    HOOK_ALL = { order: 1002, filter: { fake: null } },
+    HOOK_FAKE_LAST = { order: 10000000, filter: { fake: true } },
+    HOOK_TRUE_LAST = { order: 10000000, filter: { fake: false } },
+    HOOK_TRUE_ALL = { order: 10000000, filter: { fake: null } }
+
+const magic = '\u200b'
+const magicspace = '\u2005'
+
+module.exports = function nomorecensor(mod) {
+    const command = mod.command || mod.require.command;
+    let badwords = []
+
+    mod.queryData('/AbuseLetterList/').then(result => {
+        badwords = result.children.map(x => x.attributes.string.trimEnd())
+    })
+
+    const CHAT_SERVER_PACKETS = [['S_CHAT', 3], ['S_WHISPER', 3], ['S_PRIVATE_CHAT', 1]];
+    const CHAT_CLIENT_PACKETS = [['C_WHISPER', 1], ['C_CHAT', 1]];
+    for (let [packet, version] of CHAT_SERVER_PACKETS) mod.hook(packet, version, { order: 100000000, fake: null }, event => uncensorPacket(packet, event))
+    for (let [packet, version] of CHAT_CLIENT_PACKETS) mod.hook(packet, version, { order: 100000000, fake: null }, event => uncensorPacket(packet, event))
+
+    const uncensorPacket = (packet, event) => {
+        if (packet == "C_CHAT" && event.channel == 18) return
+        let uncensored = uncensor(event.message)
+        if (!uncensored || uncensored==event.message) return
+        event.message = uncensored
+        return true
+    }
+
+    const uncensor = (str, debug = false) => {
+        for (let i = 0; i < badwords.length; i++) {
+            if(i>0 && badwords[i]==badwords[i-1]) continue
+            let position = str.indexOf(badwords[i])
+            let count = 0
+            while (position !== -1) {
+                count++
+                str = str.substr(0, position + 1) + magic + str.substr(position + 1)
+                position = str.indexOf(badwords[i], position + 2)
+                if (count > 100) break
+            }
+            if (debug && count > 0) {
+                console.log(str.split('').map(x => `${x}:${x.charCodeAt(0)}`))
+                console.log(`badword ${badwords[i]} found ${count} times`)
+            }
+        }
+        return str
+    }
+
+
+    this.destructor = () => {
+    };
+}
